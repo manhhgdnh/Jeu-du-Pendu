60d76b45032c94c06bcdd896f81fd4b16801ef68
manhhgdnh <145620758+manhhgdnh@users.noreply.github.com>
2024-10-18 07:21:25 +0200

    Update main.pas
    update main using SDL2

--- main.pas
+++ main.pas
@@ -1,12 +1,56 @@
 program main;
 
-uses crt, partie, parametres, typeDonnees, animation;
+uses crt, partie, parametres, typeDonnees, animation, SDL2, SDL2_ttf;
 
 var input: Integer;
 var mode: TDifficulte;
 var theme: TTheme;
+var window: PSDL_Window;
+var renderer: PSDL_Renderer;
+var font: PTTF_Font;
 
 begin
+    //Initialise SDL
+    if SDL_Init(SDL_INIT_VIDEO) < 0 then
+    begin
+        writeln('Error initializing SDL: ', SDL_GetError);
+        exit;
+    end;
+
+    if TTF_Init() = -1 then
+    begin
+        writeln('Error initializing SDL_ttf: ', TTF_GetError);
+        SDL_Quit;
+        exit;
+    end;
+
+    window := SDL_CreateWindow('Jeu du Pendu', SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
+    if window = nil then
+    begin
+        writeln('Error creating window: ', SDL_GetError);
+        SDL_Quit;
+        exit;
+    end;
+    
+    renderer := SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED or SDL_RENDERER_PRESENTVSYNC);
+    if renderer = nil then
+    begin
+        writeln('Error creating renderer: ', SDL_GetError);
+        SDL_DestroyWindow(window);
+        SDL_Quit;
+        exit;
+    end;
+
+    font := TTF_OpenFont('Arial.ttf', 26);
+    if font = nil then
+    begin
+        writeln('Error loading font: ', TTF_GetError);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit;
+        exit;
+    end;
+    
     theme := dark;
 
     repeat
@@ -25,7 +69,7 @@ begin
             if input = 1 then
                 begin
                     modeDifficulte(mode);
-                    partieJeu(mode);
+                    partieJeu(mode,renderer,font); //Ajouter renderer et font
                 end
             else if input = 2 then
                 parametresJeu(theme)
@@ -38,4 +82,10 @@ begin
         end;
 
     until input = 3;
+
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit;
+    SDL_Quit;
 end.
